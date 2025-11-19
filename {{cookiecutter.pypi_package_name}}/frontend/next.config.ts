73670dbd4896cdebdd7d8a5433c4cd90c3fb7c04
Ricardo Ocampo <ricardo.ocampo@graphitehq.com>
2025-11-18 21:57:40 -0600

    Update Dockerfile and docker-compose.yml

--- {{cookiecutter.pypi_package_name}}/frontend/next.config.ts
+++ {{cookiecutter.pypi_package_name}}/frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // TODO: Investigate what this does
 };
 
 export default nextConfig;
